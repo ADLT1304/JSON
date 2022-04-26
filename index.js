3f78c86a4724b274e374a0036c6ae13f7337f1f6
Alex <banditmexican666@gmail.com>
2022-04-26 14:13:42 -0700

    Finished assignemnt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //convert the following objects to JSON
 
-let power={voltage: 250,current: 12}
+let power = {"voltage": 250, "current": 12}
 
-let car={make: "Honda", model: "Accord", year: 2017}
+let car = {"make": "Honda", "model": "Accord", "year": 2017}
 
-let person={name: "Bob", age:45, email:"jim@email.com", occupation: "teacher"}
+let person = {"name": "Bob", "age":45, "email":"jim@email.com", "occupation": "teacher"}
 
 // To illustrate how to use the JSON editor Tool we are going to create a JSON string with an array of 3 objects.The objects are:
 
@@ -12,12 +12,15 @@ let person={name: "Bob", age:45, email:"jim@email.com", occupation: "teacher"}
 
 // name=John,DOB=1/1/2000,Age=20
 
-// Object 2 contains the following key/value pairs:
+const users = `[
+{"name": "John", "age":20, "dob": "1/1/2000"},
 
-// name=Jim,DOB=21/2/1990,Age=30
+{"name": "Jim", "age":30, "dob": "21/2/1990"},
 
-// Object 3 contains the following key/value pairs: 
-
-//name=Jane,DOB=6/11/1958,Age=61
-
+{"name": "Jane", "age":61, "dob": "6/11/1958"}
+]`
 //console.log the name in each object using JSON.parse
+
+console.log(JSON.parse(users)[0].name)
+console.log(JSON.parse(users)[1].name)
+console.log(JSON.parse(users)[2].name)
